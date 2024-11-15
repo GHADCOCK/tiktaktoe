c7801da42207456d813e46be4280c415f7361d05
glennh2023 <glenn.hadcock@gmail.com>
2024-11-15 17:06:26 -0600

    ADDED EXAMPLE BOARD

---
+++ src/components/TikTakToeBoard.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState, Text, View, Button, Alert } from "react";
+
+export default function TikTakToeBoard(props) {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>TikTakToe Game!</h1>
+      <p>Just a TikTakToe board!</p>
+    </div>
+  );
+}
